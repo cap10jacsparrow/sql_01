453891ef9b3615ccb245150e1c9eedb44a721f4b
Your Name <you@example.com>
2019-02-27 15:29:51 +0530

    sql commit 2

--- 01_sql.py
+++ 01_sql.py
@@ -3,14 +3,14 @@
 import sqlite3
 
 # create a new database if the database doesn't already exist
-conn = sqlite3.connect("cars.db")
+conn = sqlite3.connect("new.db")
 
 # get a cursor object used to execute SQL commands
 cursor = conn.cursor()
 
 # create a table
-cursor.execute("""CREATE TABLE cars
-                (make TEXT, model TEXT, quantity INT)
+cursor.execute("""CREATE TABLE population
+                (city TEXT, state TEXT, population INT)
               """)
 
 # close the database connection
